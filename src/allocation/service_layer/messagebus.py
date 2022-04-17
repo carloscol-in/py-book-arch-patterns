e2c5cafba73c28d3b4ae75f6c92d4f867fefdcc2
Carlos Colin <carloscolinn@outlook.es>
2022-04-17 11:39:18 -0500

    Refactored events handler

--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -1,10 +1,15 @@
 import email
 from allocation.domain import events
+from allocation.service_layer import unit_of_work
 
 
-def handle(event: events.Event):
-    for handler in HANDLERS[type(event)]:
-        handler(event)
+def handle(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
+    queue = [event]
+    while queue:
+        event = queue.pop(0)
+        for handler in HANDLERS[type(event)]:
+            handler(event, uow=uow)
+            queue.extend(uow.collect_new_events())
 
 def send_out_of_stock_notification(event: events.OutOfStock):
     email.send_email(
